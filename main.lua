a81d16684eda18f7410ca25bea9240ec855508aa
weakman54 <enkiigm@gmail.com>
2016-04-13 10:55:35 +0200

    Fixed input bug by switching the order of initialization

--- main.lua
+++ main.lua
@@ -4,9 +4,9 @@ made by Erik Wallin
 ]]--
 
 
+Gamestate = require("libs/hump.gamestate")
 Input = require("libs/boipushy.Input")
 suit = require("libs/suit")
-Gamestate = require("libs/hump.gamestate")
 
 -- States
 require("Start_Menu")
@@ -18,14 +18,16 @@ Width, Height = love.graphics.getWidth(), love.graphics.getHeight()
 
 
 function love.load(arg)
-	-- Init input and set bindings
+	-- Init Gamestate
+    Gamestate.registerEvents()
+    Gamestate.switch(Start_Menu)
+
+    -- Init input and set bindings
     input = Input()
     
     input:bind("escape", "PauseQuit") -- Is there a better way to do this? bind as two different commands?
 
-    -- Init Gamestate
-    Gamestate.registerEvents()
-    Gamestate.switch(Start_Menu)
+
 end
 
 
