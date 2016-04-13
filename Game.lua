45a482f32f199663c66592c3f0d414719f0ad84a
weakman54 <enkiigm@gmail.com>
2016-04-13 17:27:54 +0200

    Added Init function Added grid of blocks Added drawing of grid #untested

--- Game.lua
+++ Game.lua
@@ -1,6 +1,17 @@
 
 Game = {}
 
+function Game:init()
+    Game.grid = {}
+    for y=1,20 do
+        Game.grid[y] = {}
+        for x=1,10 do
+            Game.grid[y][x] = {block=false, falling=false}
+        end
+    end
+end
+
+
 function Game:update(dt)
     -- toggle pause
     if input:pressed("PauseQuit") then
@@ -16,7 +27,14 @@ end
 
 function Game:draw(dt)
     love.graphics.setColor(255, 255, 255, 255)
-    love.graphics.rectangle("fill", 10, 10, 100, 100)
+
+    local offset = {x=100, y=30}
+
+    for y, row in ipairs(self.grid) do
+        for x, block in ipairs(row) do
+            love.graphics.rectangle("fill", x*16+offset.x, y*16+offset.y, 15, 15)
+        end
+    end
 end
 
 
